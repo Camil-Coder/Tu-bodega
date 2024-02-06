70c03efca5ab6d6eb7391ad50d95610aa6ccfdba
Camilo Castillo <cristian.c.castillo1707@gmail.com>
2024-02-06 00:25:54 -0500

    validacion de campos exitosa

--- piezasController.php
+++ piezasController.php
@@ -1,53 +1,57 @@
 <?php
-header('content-type: application/json; charset-utf-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header('content-type: application/json; charset=utf-8');
+
 require 'piezasModel.php';
 $piezasModel = new piezasModel();
 
 switch ($_SERVER['REQUEST_METHOD']) {
 
-    case 'POST': //crear
+    case 'POST': //CREATE crear
         $_POST = json_decode(file_get_contents('php://input', true));
-        if (!isset($_POST->nombre_pieza) || is_null($_POST->nombre_pieza) || empty(trim($_POST->nombre_pieza))) {
-            $respuesta = ['error', 'el nombre del vehiculo no debe estar vacio'];
-        } else if (!isset($_POST->descripcion_pieza) || is_null($_POST->descripcion_pieza) || empty(trim($_POST->descripcion_pieza))) {
-            $respuesta = ['error', 'La descripcion no debe estar vacia'];
-        } else if (!isset($_POST->precio_pieza) || is_null($_POST->precio_pieza) || empty(trim($_POST->precio_pieza))) {
-            $respuesta = ['error', 'El precio no debe estar vacio'];
+        if (!isset($_POST->nombre_pieza) || is_null($_POST->nombre_pieza) || empty(trim($_POST->nombre_pieza)) || strlen($_POST->nombre_pieza) > 80) {
+            $respuesta = ['error', 'el nombre del vehiculo no debe estar vacio y no debe superar los 80 caracteres'];
+        } else if (!isset($_POST->descripcion_pieza) || is_null($_POST->descripcion_pieza) || empty(trim($_POST->descripcion_pieza)) || strlen($_POST->descripcion_pieza) > 150) {
+            $respuesta = ['error', 'La descripcion no debe estar vacia y no debe superar los 150 caracteres'];
+        } else if (!isset($_POST->precio_pieza) || is_null($_POST->precio_pieza) || empty(trim($_POST->precio_pieza)) || !is_numeric($_POST->precio_pieza) || strlen($_POST->precio_pieza) > 20) {
+            $respuesta = ['error', 'El precio no debe estar vacio y debe ser de tipo numerico y que no supere los 20 caracteres'];
         } else {
             $respuesta = $piezasModel->postPiezas($_POST->nombre_pieza, $_POST->descripcion_pieza, $_POST->precio_pieza);
         }
         echo json_encode($respuesta);
         break;
 
-    case 'GET':  //leer
+    case 'GET':  //READ leer
         $respuesta = $piezasModel->getPiezas();
         echo json_encode($respuesta);
         break;
 
-    case 'PUT': //actualizar
+    case 'PUT': //UPLOAD actualizar
         $_PUT = json_decode(file_get_contents('php://input', true));
         if (!isset($_PUT->id) || is_null($_PUT->id) || empty(trim($_PUT->id))) {
             $respuesta = ['error', 'el id no debe estar vacio'];
-        } else if (!isset($_PUT->nombre_pieza) || is_null($_PUT->nombre_pieza) || empty(trim($_PUT->nombre_pieza))) {
-            $respuesta = ['error', 'el nombre del vehiculo no debe estar vacio'];
-        } else if (!isset($_PUT->descripcion_pieza) || is_null($_PUT->descripcion_pieza) || empty(trim($_PUT->descripcion_pieza))) {
-            $respuesta = ['error', 'La descripcion no debe estar vacia'];
-        } else if (!isset($_PUT->precio_pieza) || is_null($_PUT->precio_pieza) || empty(trim($_PUT->precio_pieza))) {
-            $respuesta = ['error', 'El precio no debe estar vacio'];
+        } else if (!isset($_PUT->nombre_pieza) || is_null($_PUT->nombre_pieza) || empty(trim($_PUT->nombre_pieza)) || strlen($_PUT->nombre_pieza) > 80) {
+            $respuesta = ['error', 'el nombre del vehiculo no debe estar vacio y no debe superar los 80 caracteres'];
+        } else if (!isset($_PUT->descripcion_pieza) || is_null($_PUT->descripcion_pieza) || empty(trim($_PUT->descripcion_pieza)) || strlen($_PUT->descripcion_pieza) > 150) {
+            $respuesta = ['error', 'La descripcion no debe estar vacia y no debe superar los 150 caracteres'];
+        } else if (!isset($_PUT->precio_pieza) || is_null($_PUT->precio_pieza) || empty(trim($_PUT->precio_pieza)) || !is_numeric($_PUT->precio_pieza) || strlen($_PUT->precio_pieza) > 20) {
+            $respuesta = ['error', 'El precio no debe estar vacio y debe ser de tipo numerico y que no supere los 20 caracteres'];
         } else {
             $respuesta = $piezasModel->putPiezas($_PUT->id, $_PUT->nombre_pieza, $_PUT->descripcion_pieza, $_PUT->precio_pieza);
         }
         echo json_encode($respuesta);
         break;
 
-    case 'DELETE';  //eliminar
+    case 'DELETE';  // DELETE eliminar
         $_DELETE = json_decode(file_get_contents('php://input', true));
         if (!isset($_DELETE->id) || is_null($_DELETE->id) || empty(trim($_DELETE->id))) {
             $respuesta = ['error', 'el id no debe estar vacio'];
+        } else {
+            $respuesta = $piezasModel->deletePiezas($_DELETE->id);
         }
-        else{
-            $respuesta = $piezasModel-> deletePiezas($_DELETE->id);
-        }
-        echo json_encode($respuesta);        
+        echo json_encode($respuesta);
         break;
 }
--- piezasModel.php
+++ piezasModel.php
@@ -11,15 +11,20 @@ class piezasModel
 
     public function postPiezas($nombre, $descripcion, $precio)
     {
-        $sql = "INSERT INTO piezas (nombre_pieza, descripcion_pieza, precio_pieza)VALUES('$nombre', '$descripcion', '$precio')";
-        mysqli_query($this->conexion, $sql);
-        $resultado = ['enhorabuena', 'vehiculo guardado'];
+        $valida = $this->validarPiezas($nombre, $descripcion, $precio);
+        $resultado = ['error', 'ya existe una pieza con las mismas caracteristicas'];
+        if (count($valida) == 0) {
+            $sql = "INSERT INTO piezas (nombre_pieza, descripcion_pieza, precio_pieza)VALUES('$nombre', '$descripcion', '$precio')";
+            mysqli_query($this->conexion, $sql);
+            $resultado = ['enhorabuena', 'vehiculo guardado'];
+        }
         return $resultado;
     }
-    public function getPiezas()
+    public function getPiezas($id = null)
     {
+        $where = ($id == null) ? "" : " WHERE id = '$id'";
         $piezas = [];
-        $sql = "SELECT * FROM piezas";
+        $sql = "SELECT * FROM piezas " . $where;
         $registros = mysqli_query($this->conexion, $sql);
         while ($row = mysqli_fetch_assoc($registros)) {
             array_push($piezas, $row);
@@ -29,18 +34,42 @@ class piezasModel
 
     public function putPiezas($id, $nombre, $descripcion, $precio)
     {
-        $sql = "UPDATE piezas SET nombre_pieza = '$nombre', descripcion_pieza = '$descripcion', precio_pieza = '$precio' WHERE id = '$id' ";
-        mysqli_query($this->conexion, $sql);
-        $resultado = ['enhorabuena', 'vehiculo actualizado'];
+        $existencia = $this->getPiezas($id);
+        $resultado = ['error', 'No existe el producto con ID: ' . $id];
+        $validacion = $this->validarPiezas($nombre, $descripcion, $precio);
+        if (count($existencia) > 0) {
+            $resultado = ['error', 'ya existe una pieza con las mismas caracteristicas'];
+            if (count($validacion) == 0) {
+                $sql = "UPDATE piezas SET nombre_pieza = '$nombre', descripcion_pieza = '$descripcion', precio_pieza = '$precio' WHERE id = '$id' ";
+                mysqli_query($this->conexion, $sql);
+                $resultado = ['enhorabuena', 'vehiculo actualizado'];
+            }
+        }
+
         return $resultado;
     }
 
     public function deletePiezas($id)
     {
-        $sql = "DELETE FROM piezas WHERE id = '$id' ";
-        mysqli_query($this->conexion, $sql);
-        $resultado=['enhorabuena', 'haz eliminado un vehiculo'];
+        $valida = $this->getPiezas($id);
+        $resultado = ['Error', 'no existe el vehiculo con la ID: ' . $id];
+        if (count($valida) > 0) {
+            $sql = "DELETE FROM piezas WHERE id = '$id' ";
+            mysqli_query($this->conexion, $sql);
+            $resultado = ['enhorabuena', 'haz eliminado un vehiculo'];
+        }
         return $resultado;
+    }
+
+    public function validarPiezas($nombre, $descripcion, $precio)
+    {
+        $piezas = [];
+        $sql = "SELECT * FROM piezas WHERE nombre_pieza = '$nombre' AND descripcion_pieza = '$descripcion' AND precio_pieza = '$precio'  ";
+        $registros = mysqli_query($this->conexion, $sql);
+        while ($row = mysqli_fetch_assoc($registros)) {
+            array_push($piezas, $row);
+        }
+        return $piezas;
     }
 }
 
